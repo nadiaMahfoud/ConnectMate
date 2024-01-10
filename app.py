e5761469ec318c667ea3f1526e490e47919686d2
Replit user <>
2024-01-10 17:17:47 +0000

    added some files

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+COUNTRIES = [
+  {
+    'id': 1,
+    'name': 'Morocco',
+    'capital': 'Rabat',
+  },
+ {
+  'id': 2,
+  'name': 'USA',
+  'capital': 'Washington DC',
+ },
+  {
+    'id': 3,
+    'name': 'Canada',
+    'capital': 'Ottawa',
+  },
+  {
+    'id': 4,
+    'name': 'Australia',
+    'capital': 'Canberra',
+  },
+  {
+    'id': 5,
+    'name': 'France',
+    'capital': 'Paris',
+  }
+]
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', countries=COUNTRIES)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
